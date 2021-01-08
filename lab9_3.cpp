911b88d9cff2b5e11722002b94d13d536af4ec9d
khomsan <khomsan_su@cmu.ac.th>
2021-01-08 18:23:00 +0700

    complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,39 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<iomanip>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0;
+    string textline;
+    ifstream source("score.txt");
+    while (getline(source,textline)){
+
+        sum += atof(textline.c_str());
+        count++;
+    }
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << endl;
+    float Mean = sum/count;
+    count = 0;
+    sum = 0;
+
+source.close();
+source.open("score.txt");
+    while (getline(source,textline)){
+
+        sum += (atof(textline.c_str())-Mean)*(atof(textline.c_str())-Mean);
+        count++;
+        
+    }
+    cout << "Standard deviation = " << sqrt(sum/count);
+
+
+return 0;
+}
